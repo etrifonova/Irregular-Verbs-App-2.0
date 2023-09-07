7f61f12bd21ff01ff72dbed5496d7d633f71679e
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2023-09-07 14:30:42 +0300

    feat: refactor

---
+++ script.module.js
@@ -0,0 +1,54 @@
+
+import { verbs as verbs } from '../verbs.js'
+
+let infinitiveDisplay = document.querySelector('.out-1');
+let randomInfinitive;
+const buttonGen = document.querySelector('.generate');
+const buttonCheck = document.querySelector('.check-button');
+let userInput = document.querySelector('.psimple-input');
+let userInput2 = document.querySelector('.pparticiple-input');
+let message = document.querySelector('.out-2');
+
+console.log(verbs[2])
+
+buttonGen.onclick = function() {
+  randomInfinitive = verbs.map(element => element.infinitive)[Math.floor(Math.random()*verbs.length)];
+  infinitiveDisplay.innerHTML = randomInfinitive;
+  message.innerHTML = '';
+  userInput.value = '';
+  userInput2.value = '';
+}
+
+function check() {
+  let correctAnswer = verbs.find((element) => {
+      return element.infinitive == randomInfinitive
+    }).psimple;
+
+  let correctAnswer2 = verbs.find((element) => {
+      return element.infinitive == randomInfinitive
+    }).pparticiple;
+
+
+    if (userInput.value.trim().toLowerCase() === correctAnswer) {
+      if (userInput2.value.trim().toLowerCase() === correctAnswer2) {
+        message.innerHTML = 'Correct!';
+      } else {
+        message.innerHTML = 'Wrong!'}
+    } else {
+        message.innerHTML = 'Wrong!'
+    }
+}
+
+buttonCheck.onclick = check();
+
+// solution found here https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
+
+userInput2.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    check();
+  }
+});
